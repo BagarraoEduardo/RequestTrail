c230a4a381500fb97e2e57d1ff18d6ffd5d1c342
Eduardo Bagarrão <general@eduardobagarrao.com>
2025-01-25 01:50:43 +0000

    updated correlationId in order to use the existing correlationId, otherwise it will create a new one

--- CalledApi/CalledApi/Middelwares/CorrelationIdMiddleware.cs
+++ CalledApi/CalledApi/Middelwares/CorrelationIdMiddleware.cs
@@ -7,20 +7,38 @@ namespace CalledApi.Middelwares;
 public class CorrelationIdMiddleware
 {
     private readonly RequestDelegate _next;
+    private readonly ILogger<CorrelationIdMiddleware> _logger;
 
-    public CorrelationIdMiddleware(RequestDelegate next)
+    public CorrelationIdMiddleware(RequestDelegate next, ILogger<CorrelationIdMiddleware> logger)
     {
         _next = next;
+        _logger = logger;
     }
 
     public async Task InvokeAsync(HttpContext context)
     {
-        var correlationId = Guid.NewGuid().ToString();
+        try
+        {
+            const string correlationIdHeaderKey = "X-Correlation-ID";
+            string correlationId;
 
-        LogContext.PushProperty("CorrelationId", correlationId);
+            if (context.Request.Headers.TryGetValue(correlationIdHeaderKey, out var correlationIdHeader))
+            {
+                correlationId = correlationIdHeader.ToString();
+            }
+            else
+            {
+                correlationId = Guid.NewGuid().ToString();
+                context.Request.Headers[correlationIdHeaderKey] = correlationId;
+            }
 
-        context.Items["CorrelationId"] = correlationId;
+            LogContext.PushProperty("CorrelationId", correlationId);
 
-        await _next(context);
+            await _next(context);
+        }
+        catch(Exception exception)
+        {
+            _logger.LogError(exception, $"An exception has ocurred. Message: {exception.Message}");
+        }
     }
 }

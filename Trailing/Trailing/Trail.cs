419dce9e4ce8edc08de010df20ff6cfe51c6ccde
Eduardo Bagarrão <general@eduardobagarrao.com>
2025-01-27 09:15:09 +0000

    Created trailing NuGet package

---
+++ Trailing/Trailing/Trail.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace CalledApi.Trailing;
+
+public class Trail
+{
+    public long Id { get; set; }
+    public DateTime RequestTimestamp { get; set; }
+    public string RequestUri { get; set; }
+    public string RequestHeaders { get; set; }
+    public string RequestBody { get; set; }
+    public DateTime? ResponseTimestamp { get; set; }
+    public string ResponseBody { get; set; }
+    public string StatusCode { get; set; }
+    public Guid CorrelationId { get; set; }
+
+}
